df96e4bba5e095e2ebe7b78d08bf9dcd9d0e96d3
rashedpine83 <rashedpine83@gmail.com>
2026-03-15 14:14:44 +0600

    Created react file

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Countries from './Components/Countries/Countries'
+
+function App() {
+  
+
+  return (
+    <>
+        <div>
+          <h1>Get started</h1> 
+        </div>
+        <Countries></Countries>
+        
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Countries/Countries.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h2>This is my Country</h2>
+        </div>
+    );
+};
+
+export default Countries;
